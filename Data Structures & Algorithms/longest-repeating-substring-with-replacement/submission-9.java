29a7b6dd0b6ae56e117407d5d6aa420fcffbf0dd
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-04-24 21:55:02 +0200

    Add: longest-repeating-substring-with-replacement - submission-9

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-9.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int l = 0 ; 
+        Set<Character> set  =new HashSet<>();
+        int a = 0;
+        int res=0 ;
+        for (int r=0 ; r<s.length(); r++){
+            if(s.charAt(l)==s.charAt(r)){
+                continue ;
+                
+
+            }
+            else {
+                if (!(a==k)){
+                    a++;
+
+                }
+                else {
+                    res=Math.max(res,r-l);
+                    while(s.charAt(l)!=s.charAt(r)){
+                        l++;
+                    }
+                    a=0;
+
+                }
+            }
+        }
+        return Math.max(s.length()-l,res);
+    }
+}

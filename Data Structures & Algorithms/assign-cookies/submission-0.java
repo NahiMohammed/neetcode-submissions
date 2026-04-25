99bb7f12d2c8d56baa08178a9423c90f20c32223
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-04-25 13:14:32 +0200

    Add: assign-cookies - submission-0

---
+++ Data Structures & Algorithms/assign-cookies/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int i = 0;
+        int j = 0;
+        int res = 0;
+        while (j<s.length && i<g.length){
+            if (s[j]>=g[i]){
+                j++;
+                i++;
+                res++;
+            }
+            else{
+                j++;
+            }
+        }
+        return res ; 
+
+    }
+}

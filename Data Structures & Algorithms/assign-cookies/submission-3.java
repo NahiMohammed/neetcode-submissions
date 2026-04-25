1a35bc0ce4a7c3dd8a13d12eadb33beffedb47c8
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-04-25 13:16:55 +0200

    Add: assign-cookies - submission-3

---
+++ Data Structures & Algorithms/assign-cookies/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int i = 0;
+        int j = 0;
+        int res = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        while (j < s.length && i < g.length) {
+            if (s[j] >= g[i]) {
+                j++;
+                i++;
+                res++;
+            } else {
+                j++;
+            }
+        }
+        return res;
+    }
+}

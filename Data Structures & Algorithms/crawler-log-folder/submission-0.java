04c4e2983f8a51c846c42f94811eb83a8ed3723f
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-04-29 19:03:06 +0200

    Add: crawler-log-folder - submission-0

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int depth = 0;
+
+        for (String s : logs) {
+            if (s.equals("../")) {
+                if (depth > 0) depth--;
+            } 
+            else if (s.equals("./")) {
+                // rien
+            } 
+            else {
+                depth++;
+            }
+        }
+
+        return depth;
+    }
+}

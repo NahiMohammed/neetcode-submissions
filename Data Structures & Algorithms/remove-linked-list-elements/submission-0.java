e2a1d9d4883c05dcccca50f52ef4355ed070fb34
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-04-30 12:53:28 +0200

    Add: remove-linked-list-elements - submission-0

---
+++ Data Structures & Algorithms/remove-linked-list-elements/submission-0.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode current = dummy;
+
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next; // on supprime
+            } else {
+                current = current.next; // on avance
+            }
+        }
+
+        return dummy.next;
+    }
+}

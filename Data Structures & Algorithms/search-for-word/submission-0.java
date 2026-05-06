835106c96aec04a0342656f9131d7c9eb1935766
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-05-06 15:10:05 +0200

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -0,0 +1,49 @@
+class Solution {
+    private int row;
+    private int col;
+    private Set<Pair<Integer, Integer>> path = new HashSet<>();
+
+    public boolean exist(char[][] board, String word) {
+        row=board.length;
+        col=board[0].length;
+
+        for ( int r=0 ; r<row;r++){
+            for (int c=0; c<col;c++){
+                if(backtracking(board,word,r,c,0)){
+                    return true ;
+
+                }
+            }
+        }
+        return false;    
+                
+    }
+
+
+
+    private boolean backtracking(char[][] board, String word,int r, int c, int indice ){
+        if (indice==word.length()){
+            return true;
+        }
+
+
+        if (r>=row || c>=col||r<0||c<0||board[r][c] != word.charAt(indice) ||
+            path.contains(new Pair<>(r, c))){ 
+            return false ;
+        }
+        if (board[r][c]==word.charAt(indice)){
+            path.add(new Pair<>(r, c));
+            boolean res =
+            backtracking(board, word,r+1,c,indice+1 )||
+            backtracking(board, word,r-1,c,indice+1 )||
+            backtracking(board, word,r,c+1,indice+1 )||
+            backtracking(board, word,r,c-1,indice+1 );
+            path.remove(new Pair<>(r, c));
+            return res;
+
+
+        }
+        return false ;
+
+    }
+}

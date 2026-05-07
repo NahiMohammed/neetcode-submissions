3ed23a9e76caaa0118757872cca8441cd3cf5a85
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-05-08 00:02:43 +0200

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i =0 ;i<trust.length;i++){
+            map.put(trust[i][1], map.getOrDefault(trust[i][1], 0) + 1);
+        }
+        int target =n-1;
+        int j =-1;
+        for (int key : map.keySet()) {
+            if (map.get(key) == target) {
+                j=key;
+            }
+        }
+        if (j==-1  ){
+            return -1;
+        }
+        for(int i =0 ;i<trust.length;i++){
+            if (trust[i][0]==j){
+                return -1;
+            }
+        }
+        return j ;
+        
+
+        
+    }
+}

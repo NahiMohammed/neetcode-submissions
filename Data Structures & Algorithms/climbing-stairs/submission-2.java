2bf689fa9871f4b63f191278e1ebe80cb2915d0d
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-05-12 21:56:53 +0200

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        return dp(n) ;
+
+        
+        
+    }
+
+    private int dp(int n ){
+        if(n<=0){
+            return 1;
+        }
+        else if (n==1){
+            return 1;
+        }
+        return dp(n-2)+dp(n-1);
+    }
+}

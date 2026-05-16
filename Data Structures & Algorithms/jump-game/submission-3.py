70e98964873f7f1aa7572ce8919161240c3b3788
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-05-16 12:19:34 +0200

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        dp=[False]*len(nums)
+        dp[len(nums)-1]=True
+        for i in range(len(nums)-2,-1,-1):
+            
+            dp[i]=(dp[i+nums[i]]==True)
+        print(dp)
+        return dp[0]==True
+
+        
+#[0,0,0,1,0]

bfa0247d69bd1f50a7a0dd9cbac8cc23df2d0e46
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-05-26 02:04:14 +0200

    Add: combinations-of-a-phone-number - submission-6

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-6.py
@@ -0,0 +1,37 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        dic = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r","s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+        res=[]
+        if digits=="":
+            return []
+        def b(idx,sub):
+            if idx==len(digits):
+                if len(sub)==len(digits):
+
+                    res.append(sub)
+                return 
+            for i in range(len(dic[digits[idx]])):
+                b(idx+1,sub+dic[digits[idx]][i])
+
+
+        """
+            for i in range(idx,len(digits)) :
+                b(i+1,0,sub+dic[digits[i]][j])
+                if j==len(dic[digits[i]]):
+                    b(i+1,0,sub+dic[digits[i]][j])
+
+                if j+1<len(dic[digits[i]]):
+                    b(i,j+1,sub)
+        """
+        b(0,"")
+        return res
+        

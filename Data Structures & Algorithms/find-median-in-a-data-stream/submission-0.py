82427fae7a78b8b558660d5e6aefa6d7545de3e2
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-05-29 17:30:11 +0200

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.py
@@ -0,0 +1,15 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.heap=[]
+        
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.heap,num)
+        
+
+    def findMedian(self) -> float:
+        if len(self.heap)%2==1 :
+            return float(self.heap[len(self.heap)//2])
+        return  (self.heap[len(self.heap)//2]+self.heap[len(self.heap)//2-1])/2
+        

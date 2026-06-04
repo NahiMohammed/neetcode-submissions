9b95cbb76f8665eebde7794d8566f47894809d96
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-06-04 20:23:03 +0200

    Add: buildings-with-an-ocean-view - submission-0

---
+++ Data Structures & Algorithms/buildings-with-an-ocean-view/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        q=deque([len(heights)-1])
+        for i in range(len(heights)-2,-1,-1):
+            if heights[i]>heights[q[0]]:
+                q.appendleft(i)
+        return list(q)
+        
+        

d0db00e817211d5dcc24684d50e2f6f966949fca
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-07-06 18:46:02 +0200

    Add: is-subsequence - submission-6

---
+++ Data Structures & Algorithms/is-subsequence/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        a=[0]*26
+        b=[0]*26
+        for i in range(len(s)) :
+            a[ord(s[i])-ord("a")]+=1
+        for i in range(len(t)) :
+            b[ord(t[i])-ord("a")]+=1
+        print(a)
+        print(b)
+        for i in range(26):
+            if a[i]!=0 and a[i]>b[i] :
+                return False
+
+        return True
+
+        

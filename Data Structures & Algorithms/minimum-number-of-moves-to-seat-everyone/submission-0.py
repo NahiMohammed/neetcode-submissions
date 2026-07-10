4d80d4e6ffbd2c0073310b586187f8d719e4598e
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-07-10 10:40:16 +0200

    Add: minimum-number-of-moves-to-seat-everyone - submission-0

---
+++ Data Structures & Algorithms/minimum-number-of-moves-to-seat-everyone/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        res=0
+        students.sort()
+        seats.sort()
+        for i in range(len(seats)):
+            if seats[i]!=students[i]:
+                res+=abs(students[i]-seats[i])
+        return res
+
+        
+[1,4,5,9]
+[1,2,3,6]

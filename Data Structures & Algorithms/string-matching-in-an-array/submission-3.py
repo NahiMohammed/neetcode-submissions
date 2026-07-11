a56a386bad237d279097df471db6ed3c7ac38d1c
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-07-11 14:14:52 +0200

    Add: string-matching-in-an-array - submission-3

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        res=[]
+        def isSubstring(s1, s2):
+            if len(s1) > len(s2):
+                return False
+
+            for i in range(len(s2) - len(s1) + 1):
+                j = 0
+                while j < len(s1) and s2[i + j] == s1[j]:
+                    j += 1
+                if j == len(s1):
+                    return True
+
+            return False
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i==j :
+                    continue 
+                else :
+                    if isSubstring(words[i],words[j]):
+                        res.append(words[i])
+        return res
+
+        

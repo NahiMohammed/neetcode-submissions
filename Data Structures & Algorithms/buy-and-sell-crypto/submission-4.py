a5d2ebf4e8bf05d3214046b4dbac50fd0b8d36b9
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-07-12 14:09:10 +0200

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res=0
+        lowest_price=prices[0]
+        for i , p in enumerate(prices):
+            res=max(res,p-lowest_price)
+            if p<lowest_price :
+                lowest_price=p
+        return res
+
+
+        

05af52c0a6ea554b2bb3a39ea732e0e23ef3f9d3
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-07-13 12:20:28 +0200

    Add: simplify-path - submission-3

---
+++ Data Structures & Algorithms/simplify-path/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def simplifyPath(self, s: str) -> str:
+        words = [x for x in s.split("/") if x]
+        print(words)
+        stack=[]
+        for x in words :
+            if x[0]==".":
+                continue
+            elif  x[0]=="..":
+                if stack :
+                    stack.pop()
+            else :
+                stack.append(x)
+        return "/"+"/".join(stack)
+
+         
+
+        

3f3e89376f6b3a5a835a1c0d7e66cd7c4a4cfb61
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-07-20 14:37:05 +0200

    Add: climbing-stairs - submission-11

---
+++ Data Structures & Algorithms/climbing-stairs/submission-11.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        res=[0]*n
+        res[0]=1
+        res[1]=2
+        for i in range(2,n):
+            res[i]=res[i-1]+res[i-2]
+        return res[n-1]
+"""
+res=[1,2,3,5,5]
+1,1,1,1,1
+1,1,1,2
+1,1,2,1
+1,2,1,1
+1,2,2
+2,1,1,1
+2,1,2
+2,2,1
+
+"""

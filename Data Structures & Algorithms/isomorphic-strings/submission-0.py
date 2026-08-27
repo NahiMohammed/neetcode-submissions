9f031111bae3176b0d5901cd97610757dddefbee
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-08-27 20:51:14 +0200

    Add: isomorphic-strings - submission-0

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        done=set()
+        new=s
+        if len(s)!=len(t) :
+            return False
+        for i in range(len(s)) :
+            if s[i] in done :
+                continue
+            else :
+
+                new =new.replace(s[i],t[i])
+                done.add(s[i])
+        return new==t
+                
+             
+        

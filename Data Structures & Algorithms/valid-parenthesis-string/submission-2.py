94396b16a5e708ec7af0d2dc9c7f330c49f895dd
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-09-03 19:55:18 +0200

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        open=0
+        closed=0
+        joker=0
+        for c in s :
+            if c=="*" :
+                joker+=1
+            elif c=="(" :
+                open+=1 
+            else :
+                if open==0 :
+                    if joker ==0 :
+                        return False
+                    else :
+                        joker-=1
+                else :
+                    open-=1
+        return (not open) or joker>=open
+
+        

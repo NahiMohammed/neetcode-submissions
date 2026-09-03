5047b4203e0b56256f03b38c57c4e9b4100d7400
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-09-03 16:30:38 +0200

    Add: subtree-of-a-binary-tree - submission-4

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-4.py
@@ -0,0 +1,71 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:  
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+
+        qu1 = deque([p])
+        qu2 = deque([q])
+
+        while qu1 and qu2:
+            n1 = qu1.popleft()
+            n2 = qu2.popleft()
+
+            if n1.val != n2.val:
+                return False
+
+            # Left children
+            if n1.left and n2.left:
+                qu1.append(n1.left)
+                qu2.append(n2.left)
+            elif n1.left or n2.left:
+                return False
+
+            # Right children
+            if n1.right and n2.right:
+                qu1.append(n1.right)
+                qu2.append(n2.right)
+            elif n1.right or n2.right:
+                return False
+
+        return True 
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+        if not subRoot:
+            return True
+
+        if not root:
+            return False
+
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+
+            if node.val == subRoot.val:
+                if self.isSameTree(node, subRoot):
+                    return True
+
+            if node.left:
+                q.append(node.left)
+
+            if node.right:
+                q.append(node.right)
+
+        return False
+                
+
+                
+
+
+
+        

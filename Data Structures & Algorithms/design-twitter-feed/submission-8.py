ba37594464d4eddc5a2d65c0debd433bf0c5b326
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-09-05 18:59:11 +0200

    Add: design-twitter-feed - submission-8

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-8.py
@@ -0,0 +1,44 @@
+class Twitter:
+
+    def __init__(self):
+        self.followers=defaultdict(set)
+        self.time=0
+        self.posts=defaultdict(list)
+
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if userId not in self.followers[userId]:
+            self.followers[userId].add(userId)
+        self.time+=1
+        self.posts[userId].append([tweetId,self.time])
+        
+
+            
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        heap=[]
+        res=[]
+        for f in self.followers[userId] :
+            post=self.posts[f]
+            for i in range(min(10,len(post))): 
+                heapq.heappush(heap,(-post[i][1],post[i][0]))
+        for _ in range(min(10,len(heap))) : 
+            _ , id = heapq.heappop(heap)
+            res.append(id)
+        return res
+
+        
+
+        
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.followers[followerId]:
+            self.followers[followerId].add(followerId)
+
+
+        self.followers[followerId].add(followeeId)
+
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.followers[followerId].discard(followeeId)
+

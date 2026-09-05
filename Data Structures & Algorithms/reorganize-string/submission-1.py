7870c0b685479c2ef1fbae08375b44f358ccd356
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-09-05 21:22:09 +0200

    Add: reorganize-string - submission-1

---
+++ Data Structures & Algorithms/reorganize-string/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = Counter(s)
+        heap=[]
+        res=[]
+        for k , v in count.items():
+            heapq.heappush(heap,(-v,k))
+        prev = None
+        prev_occ= 0
+
+        while heap :
+            freq , letter =heapq.heappop(heap)
+            if prev :
+                if heap :
+                    heapq.heappush(heap,(prev_occ,prev))
+                else :
+                    return ""
+            if res and res[-1]==letter :
+                prev=letter
+                prev_occ=freq
+
+            else : 
+                res.append(letter)
+                freq+=1
+                if freq!=0 : 
+                    heapq.heappush(heap,(freq,letter))
+         
+        return "".join(res)

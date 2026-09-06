d9a64b1e0186ec82ce60c065a6386a1733471993
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-09-06 18:08:53 +0200

    Add: island-perimeter - submission-5

---
+++ Data Structures & Algorithms/island-perimeter/submission-5.py
@@ -0,0 +1,37 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        n=len(grid)
+        m=len(grid[0])
+        start=(0,0)
+        for i in range(n):
+            for j in range(m) :
+                if grid[i][j]==1 :
+                    start=(i,j)
+                    break
+        print(start)
+        res=0
+        directions = [(0,1),(0,-1), (1,0) ,(-1,0)]
+        visited=set()
+        queu=deque([start])
+        while queu :
+            print(visited)
+            x,y = queu.popleft()
+            visited.add((x,y))
+            for dx, dy in directions :
+                nx, ny =dx+x,dy+y
+                if nx<0 or nx>=n or ny<0 or ny>=m:
+                    if nx<0 or nx>=n :
+                        res+=1
+                    elif ny<0 or ny>=m :
+                        res+=1
+                else   :
+                    if grid[nx][ny]==1:
+                        if (nx,ny) not in visited :
+                            queu.append((nx,ny))
+                    else :
+                        res+=1
+        return res
+
+
+
+        

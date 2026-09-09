97e93bf6b4af6a1ea87d04bcf8c6c7c1495d7d76
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-09-09 16:18:52 +0200

    Add: matchsticks-to-square - submission-9

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-9.py
@@ -0,0 +1,32 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        s = sum(matchsticks)
+        if s % 4 != 0:
+            return False
+        target = s // 4
+        used = [False] * len(matchsticks)
+
+        def backtracking(n, curr):
+            
+            if n == 4:
+                return True
+            for i, match in enumerate(matchsticks):
+                if used[i]:
+                    continue
+                if curr + match > target:
+                    continue
+
+                used[i] = True
+
+                if curr + match == target:
+                    if backtracking(n + 1, 0):
+                        return True
+                else:
+                    if backtracking(n, curr + match):
+                        return True
+
+                used[i] = False
+
+            return False
+
+        return backtracking(0, 0)

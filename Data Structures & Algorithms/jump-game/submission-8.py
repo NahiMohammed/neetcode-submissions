bc9fbc0f3aa28d2c18134c66451b18a0639374bc
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-09-11 12:49:20 +0200

    Add: jump-game - submission-8

---
+++ Data Structures & Algorithms/jump-game/submission-8.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        nums[-1]=-1
+        for j in range(len(nums)-2,-1,-1) :
+            if j+nums[j]>len(nums)-1 :
+                nums[j]=-1
+            else :
+                if nums[j+nums[j]]==-1 :
+                    nums[j]=-1
+        return nums[0]==-1
+
+
+        

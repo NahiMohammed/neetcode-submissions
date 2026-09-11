bdb9aa120fd85022137c3426171845fc5ae74f5a
NahiMohammed <128036831+NahiMohammed@users.noreply.github.com>
2026-09-11 12:52:57 +0200

    Add: jump-game - submission-13

---
+++ Data Structures & Algorithms/jump-game/submission-13.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        nums[-1]=-1
+        for j in range(len(nums)-2,-1,-1) :
+            
+            
+            if j+nums[j]>len(nums)-1 :
+                for k in range(j,len(nums)):
+                    nums[k]=-1
+            else :
+                if nums[j+nums[j]]==-1 :
+                    nums[j]=-1
+        return nums[0]==-1
+
+
+        
